8f3ef2b1f51b3932548bed360ab87676ef76b73d
SzymonKopanko <01169576@pw.edu.pl>
2022-12-14 11:25:42 +0100

    Dodanie elementu diagonalnego 1

--- src/gauss.c
+++ src/gauss.c
@@ -1,32 +1,41 @@
 #include "gauss.h"
+#include <math.h>
 
-/**
- * Zwraca 0 - elimnacja zakonczona sukcesem
- * Zwraca 1 - macierz osobliwa - dzielenie przez 0
- */
-int eliminate(Matrix *mat, Matrix *b){
-    /**
-  	 * Tutaj należy umieścić właściwą implemntację.
-		 */
-	int i;
-	int j;
-	int k;
-	int n = mat->r;
-	double m;
+int eliminate(Matrix *mat, Matrix *b) {
+    int a, i, j, w;
+    double value;
+    double *tmp1;
+    for (a = 0; a < (mat->c) - 1; a++) {
+        value = fabs(mat->data[a][a]);
+        w = a;
 
-	for(i=0; i<n-1; i++){   //chodzenie po kolumnach
-		if (mat->data[i][i] == 0){
-			return 1;
-		}
-        for(j=i+1; j<n; j++){
-           	m = mat->data[j][i] / mat->data[i][i];
-            for(k=i+1; k<n; k++){
-                mat->data[j][k] -= m * mat->data[i][k];
+        //wybor elementu glownego
+        for (i = a; i < mat->r; i++) {
+            if (fabs(mat->data[i][a]) > value) {
+                value = fabs(mat->data[i][a]);
+                w = i;
             }
-            b->data[j][0] -= m * b->data[i][0];
+        }
+
+        //zamiana kolumn
+        if (w != a) {
+            tmp1 = mat->data[a];
+            mat->data[a] = mat->data[w];
+            mat->data[w] = tmp1;
+
+            tmp1 = b->data[a];
+            b->data[a] = b->data[w];
+            b->data[w] = tmp1;
+        }
+
+        //zerowanie kolumny
+        for (i = a + 1; i < mat->r; i++) {
+            value = (mat->data[i][a]) / (mat->data[a][a]);
+            for (j = a; j < mat->c; j++) {
+                mat->data[i][j] = mat->data[i][j] - value * (mat->data[a][j]);
+            }
+            b->data[i][0] = b->data[i][0] - value * (b->data[a][0]);
         }
     }
-	return 0;
-}
-
-
+    return 0;
+}  
